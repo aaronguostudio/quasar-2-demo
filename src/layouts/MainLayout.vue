446243d10edc204f4799620d407b9e17d89fd847
Aaron Guo <aaronguostudio@gmail.com>
2021-02-20 09:24:31 -0800

    [feature/CF-122] update title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -28,7 +28,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links - feature
+          Essential Links - feature 2
         </q-item-label>
 
         <EssentialLink
